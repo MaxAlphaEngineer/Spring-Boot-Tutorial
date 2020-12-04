b4e75858aa8a1435e735ecbe27bb7629cedecf9d
Muzaffar <maxalphaengineer@gmail.com>
2020-12-04 14:37:50 +0500

    Initial commit

---
+++ src/test/java/uz/valleyhill/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.valleyhill.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
